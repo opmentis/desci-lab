caf3d094ea040985e0038ca28009fcd96dc754d2
opmentis <opmentisai@gmail.com>
2024-12-10 11:46:53 +0100

    Miner

--- register.py
+++ register.py
@@ -2,7 +2,7 @@ from opmentis import register_user
 
 # Register as a miner
 wallet_address = "your_wallet_address"
-labid = "your_lab_id"
+labid = "dbc00e29-721f-40e6-b073-ec627db90115"
 role_type = "miner"
 register_response = register_user(wallet_address, labid, role_type)
 print("Registration Response:", register_response)
