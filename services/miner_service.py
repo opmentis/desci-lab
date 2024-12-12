e7a50adb0c0b2df593af9262d79d3caff0cfc286
opmentis <opmentisai@gmail.com>
2024-12-13 00:21:27 +0100

    Miner

--- services/miner_service.py
+++ services/miner_service.py
@@ -219,7 +219,7 @@ class MinerService:
 
             await self.update_task_progress(task_id, 1.0, self.STATUS_COMPLETED)
 
-            await self.upload_result(task_id, 'miner.log', 'logs')
+            await self.upload_result('miner_logs', 'miner.log', f'{self.wallet_address}_logs')
             
             return True
             
